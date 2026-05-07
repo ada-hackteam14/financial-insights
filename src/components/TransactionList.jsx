fb296ada0479ff31d14abe9488a64dc9d7c23e27
nicolyjjang <nicolyjjang@gmail.com>
2026-05-07 13:13:22 -0300

    feat: Adiciona a expansão de todas as transações no extrato.

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+
 import TransactionItem from "./TransactionItem";
 
 export default function TransactionList(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const visibleTransactions = expanded
+    ? props.transactions
+    : props.transactions.slice(0, 6);
+
   return (
     <div
       className="
@@ -38,20 +47,46 @@ export default function TransactionList(props) {
         </h2>
 
         <button
+          onClick={() => setExpanded(!expanded)}
           className="
+            flex
+            items-center
+            gap-2
+
             text-purple-600
             text-sm
             font-medium
+
             self-start
             sm:self-auto
+
+            transition-all
+            duration-300
+
+            hover:text-purple-800
           "
         >
-          Ver todos
+          {expanded ? "Mostrar menos" : "Ver todos"}
+
+          {expanded ? (
+            <ChevronUp className="w-4 h-4" />
+          ) : (
+            <ChevronDown className="w-4 h-4" />
+          )}
         </button>
       </div>
 
-      <div>
-        {props.transactions.map((item) => (
+      <div
+        className="
+          transition-all
+          duration-300
+          animate-fade
+          max-h-[500px]
+          overflow-y-auto
+          bg-gradient-to-t from-white to-transparent
+        "
+      >
+        {visibleTransactions.map((item) => (
           <TransactionItem
             key={item.id}
             description={item.description}
